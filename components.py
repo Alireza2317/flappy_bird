f7d2f78b45bbd3d8c5706af5d9860fcc177b2abd
Alireza Alavi <alireza.alavi237@gmail.com>
2025-02-01 00:53:03 +0330

    defined PipesPair class

--- components.py
+++ components.py
@@ -18,5 +18,40 @@ class Ground:
 		pg.draw.rect(screen, config.ground_color, self.rect)
 
 
-class Pipes:
-	pass
+class PipesPair:
+	def __init__(self):
+		self.init_rects()
+
+		self.passed = False
+		self.off_screen_left = False
+
+
+	def init_rects(self):
+		self.x = config.Dimensions[0]
+		self.top_y = random.randint(100, config.ground_level - 100)
+		self.bottom_y = self.top_y + config.pipe_gap
+
+		self.top_rect = pg.Rect(
+			self.x, 0,
+			config.pipe_width, self.top_y
+		)
+		self.bottom_rect = pg.Rect(
+			self.x, self.top_y + config.pipe_gap,
+			config.pipe_width, self.bottom_y - config.ground_level
+		)
+
+
+	def draw(self, screen: pg.Surface):
+		pg.draw.rect(screen, config.pipe_color, self.top_rect)
+		pg.draw.rect(screen, config.pipe_color, self.bottom_rect)
+
+
+	def update(self):
+		self.x -= config.speed
+
+		if self.x + config.pipe_width <= config.player_x:
+			self.passed = True
+
+		if self.x < -config.pipe_width:
+			self.off_screen_left = True
+		
--- config.py
+++ config.py
@@ -11,9 +11,19 @@ class FlappyBirdConfig:
 	Dimensions: tuple[int, int] = (800, 650)
 	BG_COLOR: Color = (20, 20, 20)
 	fps: int = 60
+	speed: int = 3
+
+	# ground attributes
 	ground_level: int = 500
 	ground_thickness: int = 5
 	ground_color: Color = (220, 220, 220)
 
+	# pipes attributes
+	pipe_width: int = 20
+	pipe_gap: int = 100
+	pipe_color: Color = (100, 100, 100)
+
+	# player attributes
+	player_x: int = 50
 
 config = FlappyBirdConfig()
