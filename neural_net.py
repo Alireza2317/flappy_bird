b3e459ade1f6c03d009ef6326c8368c9c2b0f532
Alireza Alavi <alireza.alavi237@gmail.com>
2025-02-13 03:54:57 +0330

    added docstring

--- neural_net.py
+++ neural_net.py
@@ -1,2 +1,22 @@
+import numpy as np
+from typing import Callable
+
+np.random.seed(23)
+
+
 class Network:
-	pass
+	"""
+	A very basic neural network for training the flappy-bird game.
+	There are 4 input neurons, no hidden layers and only one output neuron.
+	The 4 inputs represent:
+		- the first 3 neurons are the bird's vision:
+			- vertical distance to the top pipe
+			- vertical distance to the bottom pipe
+			- horizental distance to the pipe
+		- the bias neuron
+
+	The binary output:
+		0: no flap
+		1: flap
+	"""
+
